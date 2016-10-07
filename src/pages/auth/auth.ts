04f5438650b44a74f2b26b01539cdad09abc74fd
Colin Ogoo <c_ogoo@Colins-MacBook-Pro.local>
2016-10-07 18:40:03 +0100

    implement animation for login and navigation to new route

--- src/pages/auth/auth.ts
+++ src/pages/auth/auth.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { Home } from '../home/home';
+
 /*
   Generated class for the Auth page.
 
@@ -13,10 +15,37 @@ import { NavController } from 'ionic-angular';
 })
 export class Auth {
 
-  constructor(public navCtrl: NavController) {}
+	round: boolean;
+	expand: boolean;
+
+  constructor(public navCtrl: NavController) {
+  	this.round = false;
+  	this.expand = false;
+
+  }
+
+  authUser() {
+  	console.log(1);
+  	this.round = true;
+  	setTimeout(() => {
+  		this.expand = true;
+  		setTimeout(() => {
+	  		this.navCtrl.setRoot(Home);
+	  	},300)
+  	},500)
+  }
+
+  setClass() {
+  	let classes = {
+  		round: this.round,
+  		expand: this.expand
+  	};
+  	return classes;
+  }
 
   ionViewDidLoad() {
     console.log('Hello Auth Page');
   }
+
 
 }
